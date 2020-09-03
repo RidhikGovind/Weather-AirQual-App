5bf8d46623a8d20fa9b5ff65589bbb5e30cd5d1d
RidhikGovind <ridzman2019@gmail.com>
2020-09-03 19:58:02 +0530

    created a database and stored data into it

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 const express = require('express'); //mandatory statement
+const Datastore = require('nedb');
+
 const app = express(); //storing express into a varible
 app.listen(3000, () => console.log("first listen at 3000")); //creating a localhost:3000
 app.use(express.static('public')); //express method to host a static page
 app.use(express.json({ limit: '1mb' })) //converting the incoming data json using express
 
-const database = [];
+const database = new Datastore('database.db');
+database.loadDatabase();
+
 
 app.post('/api', (request, response) => {
     console.log('yay');
     console.log(request.body);
     const coords = request.body;
-    database.push(coords);
-    console.log(database);
+    database.insert(coords);
+
+    const timestamp = Date.now();
+    coords.timestamp = timestamp;
+
     response.json({
         status: 'success',
+        timestamp: timestamp,
         latitude: coords.lat,
         longitude: coords.long
     })
