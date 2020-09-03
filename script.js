fd3dd9565353b45d21c64b5ca29938879b357a96
RidhikGovind <ridzman2019@gmail.com>
2020-09-03 17:59:44 +0530

    finished making fetch()

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const app = express(); //storing express into a varible
 app.listen(3000, () => console.log("first listen at 3000")); //creating a localhost:3000
 app.use(express.static('public')); //express method to host a static page
 app.use(express.json({ limit: '1mb' })) //converting the incoming data json using express
+
+const database = [];
+
 app.post('/api', (request, response) => {
     console.log('yay');
     console.log(request.body);
     const coords = request.body;
+    database.push(coords);
+    console.log(database);
     response.json({
         status: 'success',
         latitude: coords.lat,
