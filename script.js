87abc91ac0621ae1bf22407847336455156c71c8
RidhikGovind <ridzman2019@gmail.com>
2020-09-04 12:12:20 +0530

    added database features

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const express = require('express'); //mandatory statement
 const Datastore = require('nedb');
+const { request } = require('express');
 
 const app = express(); //storing express into a varible
 app.listen(3000, () => console.log("first listen at 3000")); //creating a localhost:3000
@@ -10,19 +11,26 @@ const database = new Datastore('database.db');
 database.loadDatabase();
 
 
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if (err) {
+            response.end();
+            return;
+        }
+
+        response.json(data)
+    })
+
+
+})
+
 app.post('/api', (request, response) => {
-    console.log('yay');
     console.log(request.body);
     const coords = request.body;
     database.insert(coords);
 
     const timestamp = Date.now();
-    coords.timestamp = timestamp;
+    coords.timestamp = timestamp; //creating a timestamp object inside coords
+    response.json(coords);
 
-    response.json({
-        status: 'success',
-        timestamp: timestamp,
-        latitude: coords.lat,
-        longitude: coords.long
-    })
 })
