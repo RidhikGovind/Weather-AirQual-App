6d79661744d3b7e9ecb0fe06ce56983902d638a6
RidhikGovind <ridzman2019@gmail.com>
2020-09-10 19:23:56 +0530

    created route parameters

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const express = require('express'); //mandatory statement
 const Datastore = require('nedb');
-const { request } = require('express');
+const fetch = require('node-fetch');
+
 
 const app = express(); //storing express into a varible
 app.listen(3000, () => console.log("first listen at 3000")); //creating a localhost:3000
@@ -11,7 +12,7 @@ const database = new Datastore('database.db');
 database.loadDatabase();
 
 
-app.get('/api', (request, response) => {
+app.get('/database', (request, response) => {
     database.find({}, (err, data) => {
         if (err) {
             response.end();
@@ -34,3 +35,22 @@ app.post('/api', (request, response) => {
     response.json(coords);
 
 })
+
+//making of a proxy server with an endpoint in order to access the lat and long from index.html
+
+
+app.get('/weather/:latlong', async (request, response) => {
+    console.log(request.params)
+    const latlong = request.params.latlong.split(','); //here we are getting the lat and long through the route parameters.
+    //but here the route parmeters returns 3 params but we only need 2 of them i.e latlong[0] and latlong[1]
+    console.log(latlong)
+    const lat = latlong[0];
+    const long = latlong[1];
+    console.log(lat, long);
+    const API_KEY = '2a25732c5741a2fd2f2b0de7f847f36d';
+    const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely,hourly,daily&appid=${API_KEY}`;
+    //const api_url = `https://api.openweathermap.org/data/2.5/onecall?lat=10.85&lon=76.27&exclude=minutely,hourly,daily&appid=${API_KEY}`;
+    const api_response = await fetch(api_url);
+    const api_data = await api_response.json();
+    response.json(api_data);
+});
