03fd0a65e4a5ece735059a9651eb2a86d7bbcaf3
RidhikGovind <ridzman2019@gmail.com>
2020-09-12 10:28:15 +0530

    added port

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express'); //mandatory statement
 const Datastore = require('nedb');
 const fetch = require('node-fetch');
+const port = process.env.port || 3000
 
 
 const app = express(); //storing express into a varible
-app.listen(3000, () => console.log("first listen at 3000")); //creating a localhost:3000
+app.listen(port, () => console.log(`Listening at ${port}`)); //creating a localhost:3000
 app.use(express.static('public')); //express method to host a static page
 app.use(express.json({ limit: '1mb' })) //converting the incoming data json using express
 
